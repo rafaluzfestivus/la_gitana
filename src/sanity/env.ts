1b7c2365cebd06c2eb25522e0014a97a022eb58d
Rafael <contato@apolloeventos.com.br>
2026-02-02 23:29:50 -0300

    Fix typescript error in sanity config variables

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,9 +1,6 @@
 export const apiVersion =
     process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-02-04'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-
 export function assertValue<T>(v: T | undefined, errorMessage: string): T {
     if (v === undefined) {
         throw new Error(errorMessage)
@@ -11,3 +8,13 @@ export function assertValue<T>(v: T | undefined, errorMessage: string): T {
 
     return v
 }
+
+export const dataset = assertValue(
+    process.env.NEXT_PUBLIC_SANITY_DATASET,
+    'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+)
+
+export const projectId = assertValue(
+    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
